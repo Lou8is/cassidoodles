7ea3875552c1943c43316b1076d29de6905379c1
Lou8is <lis.gpublic@gmail.com>
2024-05-13 15:35:24 +0200

    Question from 2024-05-13 removed prints

--- 2024_05_13.py
+++ 2024_05_13.py
@@ -5,14 +5,11 @@ def maxProduct(list):
         return list[0]*list[1]*list[2]
     
     list.sort()
-    print(list)
 
     candidates = list[-3:]
     if list[0]<0 and list[1]<0 and list[0]*list[1] > candidates[0]*candidates[1]:
         candidates[0] = list[0]
         candidates[1] = list[1]
-    
-    print(candidates)
 
     return candidates[0]*candidates[1]*candidates[2]
 
